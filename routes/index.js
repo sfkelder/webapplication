bb137c5d6d19af3604eccc0aacfd6679ec8b9b8b
Sjoerd Kelder <sjoerdkelder@MacBook-Air-van-Sjoerd.local>
2020-03-29 15:01:04 +0200

    fixed last issues with login

--- routes/index.js
+++ routes/index.js
@@ -31,6 +31,45 @@ router.get('/', function(req, res, next) {
                          showLogin: req.session.showLogin});
 });
 
+router.post('/login', function(req, res, next){
+
+    var sqlite3 = require("sqlite3").verbose();
+    let db = new sqlite3.Database(file);
+
+    db.serialize(function() {
+        db.all("SELECT * FROM Students WHERE student_Mail = (?) OR student_Number = (?)", [req.body.username, req.body.username], (err, rows) => {
+        if (err) {
+            throw err;
+
+        }
+            rows.forEach((row) => {
+                if(row.password === req.body.password){
+                    console.log('suc6');
+                    req.session.username = row.first_Name + " " + row.last_Name;
+                    req.session.first_Name = row.first_Name;
+                    req.session.middle_Name = row.middle_Name;
+                    req.session.last_Name = row.last_Name;
+                    req.session.student_Number = row.student_Number;
+                    req.session.student_Mail = row.student_Mail;
+                    req.session.program = row.program;
+                    req.session.showLogin = false;
+                }
+            });
+
+             res.render('index', {username: req.session.username,
+                                   showLogin: req.session.showLogin});
+
+        });
+        db.close();
+    });
+});
+
+router.get('/logout', function(req, res, next) {
+    req.session.destroy((err)=>{
+        res.redirect('/');
+    });
+});
+
 //---------------
 
 router.get('/test', function(req, res, next){
@@ -59,48 +98,5 @@ router.get('/test', function(req, res, next){
 });
 
 //------------------
-
-router.post('/login', function(req, res, next){
-
-    var sqlite3 = require("sqlite3").verbose();
-    let db = new sqlite3.Database(file);
-
-    db.serialize(function() {
-        db.all("SELECT * FROM Students WHERE student_Mail = (?) OR student_Number = (?)", [req.body.username, req.body.username], (err, rows) => {
-        if (err) {
-            throw err;
-
-        }
-            rows.forEach((row) => {
-                if(row.password === req.body.password){
-                    console.log('suc6');
-                    req.session.username = row.first_Name + " " + row.last_Name;
-                    req.session.first_Name = row.first_Name;
-                    req.session.middle_Name = row.middle_Name;
-                    req.session.last_Name = row.last_Name;
-                    req.session.student_Number = row.student_Number;
-                    req.session.student_Mail = row.student_Mail;
-                    req.session.program = row.program;
-                    req.session.login = 'Log out';
-                    req.session.showLogin = false;
-                }
-            });
-
-             res.render('index', {username: req.session.username,
-                                   showLogin: req.session.showLogin});
-
-        });
-        db.close();
-    });
-});
-
-router.get('/logout', function(req, res, next) {
-
-       req.session.username = 'Niet ingelogd';
-       req.session.showLogin = true;
-
-    res.render('index', {username: req.session.username,
-                         showLogin: req.session.showLogin});
-});
 
 module.exports = router;
